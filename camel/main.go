1096c0b73f4ba8c605d5d09591c770b5f0903103
Amar Gill <amardeep@clearbanc.com>
2022-11-20 23:37:32 -0500

    finish camel function

--- camel/main.go
+++ camel/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var input string
 	fmt.Scanf("%s\n", &input)
-	fmt.Println("Input is:", input)
+	answer := 1 // +1 because first word is not capitalized
+
+	// a string is actually a []byte
+	for _, r := range input {
+		s := string(r)
+		if strings.ToUpper(s) == s {
+			answer++
+		}
+	}
+
+	fmt.Println(answer)
 }
